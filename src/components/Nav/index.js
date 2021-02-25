5271036870c9c66be6fd9aac3a1323d0d4d7f315
Gil Orcilla <mathnasiumcos@gmail.com>
2021-02-24 20:10:40 -0700

    delete/add modify files

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function Navbar() {
-  return <div className="navbar navbar-expand-lg navbar-dark bg-dark"></div>;
+  return <div className="navbar navbar-expand-lg navbar-dark bg-dark"><h1>Office Directory</h1></div>;
 }
 
 export default Navbar;
